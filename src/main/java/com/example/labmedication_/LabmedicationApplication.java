022ec8ab0adc210c79c382d2e24bd16c8c8051cc
gustavorsan <gustavorsantos42@gmail.com>
2023-06-25 16:46:01 -0300

    setup

---
+++ src/main/java/com/example/labmedication_/LabmedicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.labmedication_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabmedicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabmedicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labmedication_/LabmedicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labmedication_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmedicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
